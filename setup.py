a2130a6319cf43a8b9c934954d8a8b6f7e237b4c
zhirezhixin <yifeil@berkeley.edu>
2020-06-25 00:17:39 -0700

    fix: update version number

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open('README.md', 'r') as fh:
 setuptools.setup(
     name='letslog',
     packages = ['letslog'],
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     author='YiFei Li',
     author_email='yifeil@berkeley.edu',
     description='A logger package',
     url='https://github.com/yifeili98/letslog',
-    download_url='https://github.com/yifeili98/letslog/archive/0.0.2.tar.gz',
+    download_url='https://github.com/yifeili98/letslog/archive/0.0.3.tar.gz',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
